fd2435e9437eeef97f8a3106e34510b629ae0242
MEDAM VENKATA RAMI REDDY <84761697+venkat1705@users.noreply.github.com>
2022-11-10 12:55:11 +0530

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,6 +10,7 @@ export default NextAuth({
       })
     // ...add more providers here
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks:{
     async session({session,token}) {
         session.user.tag = session.user.name.split(" ").join("").toLowerCase();
